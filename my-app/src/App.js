1a6b72ef2a3ab445006fbf70be64dc1479ebdaaf
MaximBondarev1999 <103505432+MaximBondarev1999@users.noreply.github.com>
2022-10-29 15:59:43 +0300

    start a social network project

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css';
+import Profile from "./components/Profile";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <Header />
+      <Nav />
+      <Profile />
+    </div>
+  )
+
+}
+
+export default App;
